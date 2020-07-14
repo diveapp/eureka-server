2bdc11dfe63afd1a6631703140483cf5f006f2c1
lijixiao <lijixiao_job@126.com>
2020-07-14 11:41:34 +0800

    eureka-server basic completed

---
+++ src/main/java/com/happy/home/eureka/server/EurekaServerApplication.java
@@ -0,0 +1,17 @@
+package com.happy.home.eureka.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author: lijixiao
+ * @date: 2020-07-09
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class,args);
+    }
+}
